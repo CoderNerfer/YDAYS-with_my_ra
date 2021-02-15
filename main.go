0d4cfbde3ba812b417d66f87bb00b224748cec33
CoderNerfer <clementvey@orange.fr>
2021-02-15 13:51:50 +0100

    language commit

--- main.go
+++ main.go
@@ -1,44 +1,93 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"html/template"
-	"log"
+	"io/ioutil"
 	"net/http"
-	"os"
 )
 
+// JSON sstruct pour recup les element du json
 type JSON struct {
-	Title  string
-	Auteur string
-	About  string
-	Choix  string
+	Title          string
+	Link1          string
+	Link2          string
+	Link3          string
+	Link4          string
+	Link5          string
+	Home           string
+	Email          string
+	Facebook       string
+	Popup          string
+	Projet         string
+	Prensation     string
+	Span1          string
+	Span2          string
+	Span3          string
+	Span4          string
+	About1         string
+	About2         string
+	About3         string
+	About4         string
+	About5         string
+	About6         string
+	About7         string
+	About8         string
+	About9         string
+	About10        string
+	About11        string
+	Classinfo      string
+	Classanim      string
+	Classchefe     string
+	Classestratcom string
+	Classda        string
+	Clem           string
+	Mathis         string
+	Steph          string
+	Lilian         string
+	Ju             string
+	Lele           string
+	Mel1           string
+	Mel2           string
+	Lou1           string
+	Lou2           string
+	Bd             string
+	Titre          string
+	Archive        string
+	Resume1        string
+	Resume2        string
+	Resume3        string
+	Resume4        string
+	Resume5        string
 }
 
 var tpl *template.Template
 
 func main() {
 	tpl, _ = tpl.ParseGlob("template/*.html")
-	port := os.Getenv("PORT")
+	// port := os.Getenv("PORT")
 	//the diffenrent possible path
 	http.HandleFunc("/", getFormeHandler)
 	http.Handle("/public/", http.FileServer(http.Dir(".")))
-	log.Print("Listening on :" + port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
-	// http.ListenAndServe(":8080", nil)
+	// log.Print("Listening on :" + port)
+	// log.Fatal(http.ListenAndServe(":"+port, nil))
+	http.ListenAndServe(":8080", nil)
 }
 func getFormeHandler(w http.ResponseWriter, r *http.Request) {
-	// fmt.Println("In Home")
-	// jsonfile, _ := ioutil.ReadFile("language.json")
+	fmt.Println("In Home")
+	jsonfile, _ := ioutil.ReadFile("language.json")
 
-	// var data []JSON
-	// // unmarshall (tres sombre --> aller voir)
-	// json.Unmarshal(jsonfile, &data)
-
-	// choice := r.FormValue("Submit")
-	// if choice == "choix 2" {
-	// 	tpl.ExecuteTemplate(w, "index.html", data[1])
-	// } else {
-	// 	tpl.ExecuteTemplate(w, "index.html", data[0])
-	// }
-	tpl.ExecuteTemplate(w, "index.html", nil)
+	var data []JSON
+	// unmarshall (tres sombre --> aller voir)
+	json.Unmarshal(jsonfile, &data)
+	// fmt.Println(data)
+	choice := r.FormValue("submit")
+	if choice == "EN" {
+		tpl.ExecuteTemplate(w, "index.html", data[1])
+	} else {
+		tpl.ExecuteTemplate(w, "index.html", data[0])
+	}
+	// fmt.Println(r.FormValue("submit"))
+	// tpl.ExecuteTemplate(w, "index.html", nil)
 }
